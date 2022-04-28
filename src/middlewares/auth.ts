83f00d281d9c9e0802a0455a9d01ed44a055f62a
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-04-28 13:20:42 -0300

    implementado a verificação do token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
+import JWT from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const Auth = {
-   private: (req: Request, res: Response, next: NextFunction) => {
+   private: async (req: Request, res: Response, next: NextFunction) => {
       let sucess = false;
 
       // Fazer verificação de auth
+      if (req.headers.authorization) {
+
+         const [authType, token] = req.headers.authorization.split(' ');
+         if (authType === 'Bearer') {
+            try {
+               JWT.verify(
+                  token,
+                  process.env.JWT_SECRET_KEY as string
+               );
+
+               sucess = true;
+            } catch (error) {
+
+            }
+         }
+      }
+
       if (sucess) {
          next();
       } else {
